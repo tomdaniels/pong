097ad007487fc1d5e1888c5f6e5a95d05c160c2d
tdtele <thomsondaniels@hotmail.com>
2024-03-03 14:16:36 +1100

    centered offset fn

--- src/game_pieces/title.rs
+++ src/game_pieces/title.rs
@@ -2,6 +2,10 @@ use crate::constants::*;
 
 use raylib::text::measure_text;
 
+fn centered_offset(str: &str, font_size: i32) -> i32 {
+    WINDOW_WIDTH_HALF as i32 - measure_text(str, font_size) / 2
+}
+
 pub struct Title {
     pub x: i32,
     pub y: i32,
@@ -12,8 +16,8 @@ pub struct Title {
 impl Title {
     pub fn new(content: &str, font_size: i32) -> Title {
         Title {
-            x: WINDOW_WIDTH_HALF as i32 - (measure_text(&content, font_size) / 2),
             y: 20,
+            x: centered_offset(content, font_size),
             content: String::from(content),
             font_size
         }
@@ -21,6 +25,6 @@ impl Title {
 
     pub fn set_content(&mut self, title: &str) {
         self.content = String::from(title);
-        self.x = WINDOW_WIDTH_HALF as i32 - measure_text(&self.content, self.font_size) / 2;
+        self.x = centered_offset(&self.content, self.font_size);
     }
 }
